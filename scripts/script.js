8a870674d11464d4a9f3d3151060f2be3f0bdf3d
Samuel Escapa <10688964+saescapa@users.noreply.github.com>
2019-05-08 21:08:13 -0700

    Pinnalce update

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,21 @@
-let colors = [["#ff9a9e", "#fad0c4"],["#ffecd2", "#fcb69f"],["#ff9a9e", "#fecfef"],["#a1c4fd", "#c2e9fb"],["#cfd9df", "#e2ebf0"],["#fdfbfb", "#ebedee"],["#f5f7fa", "#c3cfe2"],["#667eea", "#764ba2"],["#fdfcfb", "#e2d1c3"],["#89f7fe", "#66a6ff"],["#48c6ef", "#6f86d6"],["#feada6", "#f5efef"],["#a3bded", "#6991c7"],["#13547a", "#80d0c7"],["#93a5cf", "#e4efe9"],["#434343", "#000000"],["#93a5cf", "#e4efe9"],["#ff758c", "#ff7eb3"],["#868f96", "#596164"],["#c79081", "#dfa579"],["#09203f", "#537895"],["#96deda", "#50c9c3"],["#29323c", "#485563"],["#ee9ca7", "#ffdde1"],["#1e3c72", "#2a5298"],["#ffc3a0", "#ffafbd"],["#B7F8DB", "#50A7C2"]];
-
+// let colors = [["#ff9a9e", "#fad0c4"],["#ffecd2", "#fcb69f"],["#ff9a9e", "#fecfef"],["#a1c4fd", "#c2e9fb"],["#cfd9df", "#e2ebf0"],["#fdfbfb", "#ebedee"],["#f5f7fa", "#c3cfe2"],["#667eea", "#764ba2"],["#fdfcfb", "#e2d1c3"],["#89f7fe", "#66a6ff"],["#48c6ef", "#6f86d6"],["#feada6", "#f5efef"],["#a3bded", "#6991c7"],["#13547a", "#80d0c7"],["#93a5cf", "#e4efe9"],["#434343", "#000000"],["#93a5cf", "#e4efe9"],["#ff758c", "#ff7eb3"],["#868f96", "#596164"],["#c79081", "#dfa579"],["#09203f", "#537895"],["#96deda", "#50c9c3"],["#29323c", "#485563"],["#ee9ca7", "#ffdde1"],["#1e3c72", "#2a5298"],["#ffc3a0", "#ffafbd"],["#B7F8DB", "#50A7C2"]];
+// let colors = ["#237dff", "#ebff23", "#ff3623", "#3623ff", "#23ebff", "#a523ff", "#23ffa5"]
+let colors = ["#6faaff"]
 let hours = new Date().getSeconds()%colors.length;
-document.getElementById("logo").style["background-image"] = `linear-gradient(to right, ${colors[hours][0]}, ${colors[hours][1]}`;
+// document.getElementById("logo").style["background-image"] = `linear-gradient(to right, ${colors[hours][0]}, ${colors[hours][1]}`;
 // document.getElementById("logo").style["background-size"] = "400% 400%";
 
 particlesJS("particles", {
   "particles": {
     "number": {
+      "value": 80,
       "density": {
         "enable": true,
         "value_area": 100
       }
     },
     "color": {
-      "value": colors[hours][0]
+      "value": colors[hours]
     },
     "shape": {
       "type": "circle",
@@ -50,13 +52,6 @@ particlesJS("particles", {
         "sync": false
       }
     },
-    "line_linked": {
-      "enable": false,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
     "move": {
       "enable": true,
       "speed": 2,
@@ -69,45 +64,6 @@ particlesJS("particles", {
         "enable": false,
         "rotateX": 600,
         "rotateY": 600
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "bubble"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 250,
-        "size": 0,
-        "duration": 2,
-        "opacity": 0,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 400,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
       }
     }
   },
