d010922bca4d7e11aeeab42fef30f8b4c1658b1a
Kendall Goto <k@kgo.to>
2019-11-16 13:43:34 -0500

    account for mobileview/nojs view

--- assets/bg.js
+++ assets/bg.js
@@ -2,6 +2,8 @@ var lines = [];
 window.onbeforeunload = function () {
   window.scrollTo(0, 0); //for dev
 }
+var mobile = true;
+var reduced = false;
 $(function() {
     updateLines();
     $(window).resize(function() {
@@ -11,10 +13,8 @@ $(function() {
     var animationFinished = false;
     var highestScroll = 0;
     $(window).scroll(function() {
-        //scrollspy the line ani:
-        if(!initialScroll) {
-            //start animation ...
-        }
+        if(mobile)
+            return; //don't scrollspy on mobile!!
         initialScroll = true;
         //get scroll percentage
         var scrollTop = $(window).scrollTop();
@@ -47,7 +47,22 @@ $(function() {
 function updateLines() {
     var screenHeight = $(window).height();
     var screenWidth = $(window).width();
-    var scaleRatio = 1;
+    if(screenWidth < 767) {
+        mobile = true;
+        $('html').prop('id', 'mobileView');
+    } else {
+        if(screenWidth < 1200) {
+            mobile = false;
+            reduced = true;
+            $('html').prop('id', 'reduced');
+        } else {
+            mobile = false;
+            reduced = false;
+            $('html').prop('id', '');
+        }
+    }
+    if(mobile)
+        return;
     var content_leftEdge = ($('.right-body').first().offset().left) * 0.9;
     var content_rightEdge = ($('.right-body').first().offset().left + $('.right-body').first().width()) * 1.1;
     //i'll clean this once the line positioning is finalized
@@ -65,7 +80,7 @@ function updateLines() {
         ],
         [
             [screenWidth, screenHeight * 0.8],
-            [content_rightEdge, scaleRatio * (screenHeight * 0.8 + (screenWidth - content_rightEdge))],
+            [content_rightEdge, (screenHeight * 0.8 + (screenWidth - content_rightEdge))],
             [content_rightEdge, $('#description h2').offset().top + $('#description h2').height() - content_rightEdge*0.15],
             [content_rightEdge * 0.85, $('#description h2').offset().top + $('#description h2').height()],
             [content_rightEdge * 0.7, $('#description h2').offset().top + $('#description h2').height()]
@@ -73,32 +88,37 @@ function updateLines() {
         [
             [screenWidth * 0.95, screenHeight * 0.75],
             [screenWidth * 0.95, $('#schedule h2').offset().top],
-            [screenWidth, scaleRatio* ($('#schedule h2').offset().top + (screenWidth * 0.05))]
+            [screenWidth, ($('#schedule h2').offset().top + (screenWidth * 0.05))]
         ],
         [
             [content_rightEdge * 0.7, $('#description h2').offset().top],
             [content_rightEdge, $('#description h2').offset().top],
             [content_rightEdge, $('#description .inner').offset().top + $('#description .inner').height()/2],
-            [screenWidth, scaleRatio * ($('#description .inner').offset().top + $('#description .inner').height()/2 + (screenWidth - content_rightEdge))]
+            [screenWidth, ($('#description .inner').offset().top + $('#description .inner').height()/2 + (screenWidth - content_rightEdge))]
         ],
         [
-            [content_leftEdge * 0.75, scaleRatio * ($('#schedule').offset().top + (screenWidth * 0.9 - content_rightEdge * 0.9))],
-            [screenWidth * 0.05, scaleRatio * ((scaleRatio * ($('#schedule').offset().top + (screenWidth * 0.9 - content_rightEdge * 0.9))) + (content_leftEdge * 0.75 - screenWidth * 0.05))],
+            [content_leftEdge * 0.75, ($('#schedule').offset().top + (screenWidth * 0.9 - content_rightEdge * 0.9))],
+            [screenWidth * 0.05, ((($('#schedule').offset().top + (screenWidth * 0.9 - content_rightEdge * 0.9))) + (content_leftEdge * 0.75 - screenWidth * 0.05))],
             [screenWidth * 0.05, $('.skyline').offset().top+50]
         ],
         [
             [content_rightEdge * 0.9, $('#schedule').offset().top],
-            [screenWidth * 0.9, scaleRatio * ($('#schedule').offset().top + (screenWidth * 0.9 - content_rightEdge * 0.9))],
-            [screenWidth * 0.9, $('.skyline').offset().top]
+            [screenWidth * 0.95, ($('#schedule').offset().top + (screenWidth * 0.95 - content_rightEdge * 0.9))],
+            [screenWidth * 0.95, $('.skyline').offset().top]
         ],
         [
             [screenWidth, $('#sponsors').offset().top + 100],
-            [content_rightEdge, scaleRatio * ($('#sponsors').offset().top + 100 + (screenWidth-content_rightEdge))],
+            [content_rightEdge, ($('#sponsors').offset().top + 100 + (screenWidth-content_rightEdge))],
             [content_rightEdge, $('#sponsors').offset().top + $('#sponsors').height()*0.9],
             [screenWidth, $('#sponsors').offset().top + $('#sponsors').height()*0.9]
         ]
     ];
-
+    if(reduced) {
+        lines[0] = [];
+        lines[3] = [];
+        lines[5] = [];
+        lines[6] = [];
+    }
     //canvas setup
     var canvas = $('canvas')[0];
 
@@ -109,20 +129,16 @@ function updateLines() {
     $(canvas).height($('html').height());
     var ctx = canvas.getContext("2d");
     ctx.lineWidth = 3;
-    ctx.strokeStyle = 'rgb(198, 158, 96)';
-    for(var i = 0; i < lines.length; i++) {
-        //line-level
-        var hasFirstPoint = false;
+    ctx.strokeStyle = 'rgb(198, 158, 96)'; //TODO: dynamic coloring
+    for(var i = 0; i < lines.length; i++) { //per line
         ctx.beginPath();
-        for(var j = 0; j < lines[i].length; j++) {
-            //point-level
+        for(var j = 0; j < lines[i].length; j++) { //per point
             var x = lines[i][j][0],
                 y = lines[i][j][1];
-            if(!hasFirstPoint)
+            if(j == 0)
                 ctx.moveTo(x, y);
             else
                 ctx.lineTo(x, y);
-            hasFirstPoint = true;
         }
         ctx.stroke();
     }
