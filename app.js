4c0a456bb4a1a0176291b7bfe525db04469eaad3
Kendall Goto <k@kgo.to>
2020-12-15 05:39:21 -0500

    trust proxy so morgan is nicer to us

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var app = express();
 //EJS Views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('trust proxy', true);
 
 if(process.env.NODE_ENV == 'production') {
     var accessLogStream = fs.createWriteStream(path.join(__dirname, '../', 'prod.log'), { flags: 'a' })
