ff273a7832eb92cd116e87b9a71c7f805409327d
SDBagel <sdbagel@gmail.com>
2021-04-19 12:49:19 -0700

    sp: Functional carousel navigation buttons

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -1,32 +1,39 @@
 
+<script lang="ts">
+  function scrollCarousel(direction: number): void {
+    const c = document.querySelector("#carouselContainer");
+    c.scrollBy(direction * c.getBoundingClientRect().width, 0);
+  }
+</script>
+
 <div class="container-wide light-bg" id="carousel">
   <div class="container inner">
     <h2 class="mb-2">Upcoming Hackathons</h2>
     <p class="carousel-subtitle mb-4">Participate in an upcoming partner event to qualify for Pinnacle 2021.</p>
     <div class="carousel">
-      <button class="carousel-nav">
+      <button class="carousel-nav reverse" on:click="{() => {scrollCarousel(-1);}}">
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <path d="M10,2l9,10l-9,10h2l9-10L12,2H10z"></path>
         </svg>
       </button>
-      <div class="carousel-inner flex-row flex-list">
-        <div class="carousel-element"></div>
-        <div class="carousel-element"></div>
-        <div class="carousel-element"></div>
-        <div class="carousel-element"></div>
-        <div class="carousel-element"></div>
-        <div class="carousel-element"></div>
-        <div class="carousel-element"></div>
-        <div class="carousel-element"></div>
-        <div class="carousel-element"></div>
+      <div class="carousel-inner flex-row flex-list" id="carouselContainer">
+        <div class="carousel-element">1</div>
+        <div class="carousel-element">2</div>
+        <div class="carousel-element">3</div>
+        <div class="carousel-element">4</div>
+        <div class="carousel-element">1</div>
+        <div class="carousel-element">2</div>
+        <div class="carousel-element">3</div>
+        <div class="carousel-element">4</div>
+        <div class="carousel-element">1</div>
       </div>
-      <button class="carousel-nav">
+      <button class="carousel-nav" on:click="{() => {scrollCarousel(1);}}">
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <path d="M10,2l9,10l-9,10h2l9-10L12,2H10z"></path>
         </svg>
       </button>
     </div>
-    <p class="carousel-subtitle mt-4">Check out our <a href="/hackathons">full list</a> of partnered events.</p>
+    <p class="carousel-subtitle">Check out our <a href="/hackathons">full list</a> of partnered events.</p>
   </div>
 </div>
 
@@ -64,6 +71,10 @@
     overflow: visible;
     transform: scale(1.0);
     transition: transform 0.5s ease;
+
+    &.reverse {
+      transform: scale(-1.0);
+    }
     
     svg {
       width: 80%;
@@ -75,9 +86,11 @@
     flex-wrap: nowrap;
     max-width: 100%;
     width: 100%;
-    overflow: scroll;
-    -ms-scroll-snap-type: x mandatory;
-    scroll-snap-type: x mandatory;
+
+    overflow: auto;
+    scroll-behavior: smooth;
+    -ms-scroll-snap-type: x proximity;
+    scroll-snap-type: x proximity;
 
     scrollbar-width: none;
     &::-webkit-scrollbar {
@@ -85,19 +98,23 @@
     }
 
     .carousel-element {
-      background-color: #000;
-      border-radius: 5px;
       display: block;
       height: 200px;
       min-width: calc(25% - 15px);
       scroll-snap-align: start;
-      scroll-snap-stop: always;
+      scroll-snap-stop: normal;
+
+      // Distinguishing blocks. Debug only
+      background-color: #000;
+      border-radius: 5px;
+      color: #fff;
+      font-size: 24px;
     }
   }
 
   .carousel-subtitle {
     text-align: center;
-    margin-top: 15px;
+    margin-top: 2rem;
     font-size: 1.3rem;
   }
 
