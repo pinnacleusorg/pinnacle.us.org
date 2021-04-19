756606b9594e819a331125745be50ba7cec29bd5
SDBagel <sdbagel@gmail.com>
2021-04-19 08:19:11 -0700

    style: Format and move EngagementDeck styles

--- src/components/EngagementDeck.svelte
+++ src/components/EngagementDeck.svelte
@@ -1,31 +1,106 @@
 
 <div class="card-deck engagement-deck">
   <div class="card">
-      <div class="card-body">
-          <h3 class="card-title">Future staff?</h3>
-          <p class="card-text p-2">Be a part of the team that makes it happen.</p>
-      </div>
-      <div class="card-footer">
-          <a href="https://hack.ms/P20-Team-Application" class="pinnacle-btn mt-2" id="applybtn">Apply Now</a>
-      </div>
+    <div class="card-body">
+      <h3 class="card-title">Future staff?</h3>
+      <p class="card-text p-2">Be a part of the team that makes it happen.</p>
+    </div>
+    <div class="card-footer">
+      <a
+        href="https://hack.ms/P20-Team-Application"
+        class="pinnacle-btn mt-2"
+        id="applybtn">Apply Now</a
+      >
+    </div>
   </div>
-  <div class="card card-divider">
-  </div>
+  <div class="card card-divider" />
   <div class="card">
-      <div class="card-body">
-          <h3 class="card-title">Stay in the Loop</h3>
-          <div class="stacked-input">
-              <div class="input-group">
-                  <input type="text" class="form-control" id="engagement-fn" placeholder="First Name">
-              </div>
-              <div class="input-group">
-                  <input type="email" class="form-control" id="engagement-email" placeholder="Email Address">
-              </div>
-              <div class="mt-1" id="updatedMsg">&nbsp;</div>
-          </div>
+    <div class="card-body">
+      <h3 class="card-title">Stay in the Loop</h3>
+      <div class="stacked-input">
+        <div class="input-group">
+          <input
+            type="text"
+            class="form-control"
+            id="engagement-fn"
+            placeholder="First Name"/>
+        </div>
+        <div class="input-group">
+          <input
+            type="email"
+            class="form-control"
+            id="engagement-email"
+            placeholder="Email Address"/>
+        </div>
+        <div class="mt-1" id="updatedMsg">&nbsp;</div>
       </div>
-      <div class="card-footer">
-          <a href="#submit" class="pinnacle-btn mt-2" id="updatedbtn">Stay Updated</a>
-      </div>
+    </div>
+    <div class="card-footer">
+      <a href="#submit" class="pinnacle-btn mt-2" id="updatedbtn">Stay Updated</a>
+    </div>
   </div>
 </div>
+
+<style lang="scss">
+  .card, .card-body, .card-footer {
+    background: none;
+    border: none;
+    font-family: KeplerStd;
+  }
+  .engagement-deck {
+    margin-bottom: 3rem;
+  }
+  .engagement-deck .card {
+    text-align: center;
+  }
+  .engagement-deck .card .card-body {
+    padding-bottom: 0;
+  }
+  .engagement-deck .card p.card-text {
+    font-size: 1.3rem !important;
+    font-family: CasperRegular;
+  }
+  .card.card-divider {
+    flex: 0 0 3px;
+    background-color: var(--pinnacle-gold);
+    margin-top: 40px;
+    margin-bottom: 40px;
+  }
+  #applybtn {
+    min-width: 180px;
+  }
+  #updatedbtn {
+    min-width: 180px;
+  }
+  .stacked-input {
+    width: 75%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .input-group input {
+    border-color: #1B1B1B;
+    border-width: 3px;
+    border-radius: 0;
+    color: black;
+    background-color: var(--pinnacle-bg-light);
+    font-size: 1.3rem;
+    padding: 0.5rem 0.6rem;
+  }
+  .input-group input::placeholder {
+    color: var(--pinnacle-bg);
+  }
+  .input-group input:focus, .input-group input:active {
+    outline: none;
+    box-shadow: none;
+    border-color: #1B1B1B;
+    background-color: var(--pinnacle-bg-light);
+    color: black;
+  }
+  
+  .stacked-input .input-group:first-child input {
+    border-bottom-width: 0.5px;
+  }
+  .stacked-input .input-group:last-child input {
+    border-top-width: 0.5px;
+  }
+</style>
