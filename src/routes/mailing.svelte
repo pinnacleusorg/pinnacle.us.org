0da79192adacf4861bdd3105e13352d35d5ea0f8
Kendall Goto <k@kgo.to>
2021-10-01 11:59:16 -0400

    feat: add mailing onboard via GET

---
+++ src/routes/mailing.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import Header from "../core/components/Header.svelte";
+	import Footer from "../components/Footer.svelte";
+
+	import { onMount } from "svelte";
+
+	//fetch API root
+	import { stores } from "@sapper/app";
+	const { session } = stores();
+	const { API_ROOT } = $session;
+
+	onMount(() => {
+		const urlParams = new URLSearchParams(window.location.search);
+		const userEmail = urlParams.get("email");
+		const userName = urlParams.get("name");
+		if (userEmail && userName) {
+			fetch(API_ROOT + "/contacts", {
+				method: "post",
+				body: JSON.stringify({
+					email: userEmail.trim(),
+					name: userName.trim(),
+				}),
+				headers: new Headers({ "Content-Type": "application/json" }),
+			}); // fail silently
+		}
+	});
+</script>
+
+<svelte:head>
+	<title>Pinnacle • Mailing List</title>
+</svelte:head>
+
+<div class="light-bg">
+	<Header />
+	<section class="container component-section">
+		<a href="/">&lsaquo; Home</a>
+		<h2>Welcome to the Mailing List</h2>
+		<p>
+			You've been enrolled in our mailing list, the best way to stay
+			up-to-date with the latest Pinnacle news. We don't send too many
+			emails, but if you'd wish to unsubscribe, you may follow the
+			unsubscribe link found in the footer of every email we send.
+		</p>
+	</section>
+	<Footer showLegal={false} />
+</div>
+
+<style lang="scss">
+	a {
+		text-decoration: none;
+	}
+
+	.container {
+		max-width: 900px;
+		padding-left: 30px;
+		padding-right: 30px;
+	}
+</style>
