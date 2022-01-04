459263f91f78ad1fce805cbdcce8c7284162f963
SDBagel <sdbagel@gmail.com>
2022-01-03 23:09:55 -0800

    Apply Pinnacle select styles to radio, text options

--- src/lib/pi-option-radio.svelte
+++ src/lib/pi-option-radio.svelte
@@ -4,13 +4,57 @@
   export let name: string;
 </script>
 
-<div class="input">
-  <label for="{id}"><slot /></label><br>
-  <input type="radio" id="{id}" name="{name}">
-</div>
+<label for="{id}">
+	<input id="{id}" name="{name}" type="radio">
+	<span><slot /></span>
+	<img class="checknt" src="/icon/checknt.svg" alt="unchecked">
+	<img class="checked" src="/icon/checked.svg" alt="checked">
+</label>
 
 <style lang="scss">
-  .input {
-    display: flex;
-  }
+	@import "./global.scss";
+
+	label {
+		display: flex;
+		flex-direction: row-reverse;
+		align-items: center;
+		justify-content: flex-end;
+		column-gap: 20px;
+
+		padding: 15px 0;
+		user-select: none;
+
+		img {
+			width: 20px;
+		}
+	}
+
+	input {
+		position: absolute;
+		left: -2000px;
+
+		&:focus + span {
+			text-decoration: underline;
+		}
+
+		+ span {
+			color: $gold;
+			opacity: 0.5;
+			transition-duration: 0.2s;
+		}
+
+		~ .checked {
+			display: none;
+		}
+
+		&:checked + span {
+			opacity: 1;
+		}
+		&:checked ~ .checked {
+			display: block;
+		}
+		&:checked ~ .checknt {
+			display: none;
+		}
+	}
 </style>
--- src/lib/pi-option-text.svelte
+++ src/lib/pi-option-text.svelte
@@ -2,20 +2,75 @@
 <script lang="ts">
   export let id: string;
   export let name: string;
-  export let value: string;
+  let value: string;
+
+  let inp: HTMLInputElement;
+  function forceFocus() {
+
+  }
 </script>
 
-<div class="input">
-  <label for="{id}">{value}</label><br>
-  <input type="radio" id="{id}" name="{name}" bind:value="{value}">
-  <input type="text" bind:value="{value}">
-</div>
+<label for="{id}">
+	<input type="text" bind:value>
+	<input id="{id}" name="{name}" type="radio">
+	<span><slot /></span>
+	<img class="checknt" src="/icon/checknt.svg" alt="unchecked">
+	<img class="checked" src="/icon/checked.svg" alt="checked">
+</label>
 
 <style lang="scss">
-  .input {
-    display: flex;
-  }
-  label {
-    display: none;
+	@import "./global.scss";
+
+	label {
+		display: flex;
+		flex-direction: row-reverse;
+		align-items: center;
+		justify-content: flex-end;
+		column-gap: 20px;
+
+		padding: 15px 0;
+		user-select: none;
+
+		img {
+			width: 20px;
+		}
+	}
+
+	input[type="radio"] {
+		position: absolute;
+		left: -2000px;
+
+		&:focus + span {
+			text-decoration: underline;
+		}
+
+		+ span {
+			color: $gold;
+			opacity: 0.5;
+			transition-duration: 0.2s;
+		}
+
+		~ .checked {
+			display: none;
+		}
+
+		&:checked + span {
+			opacity: 1;
+		}
+		&:checked ~ .checked {
+			display: block;
+		}
+		&:checked ~ .checknt {
+			display: none;
+		}
+	}
+
+  input[type="text"] {
+    background-color: transparent;
+    border: none;
+    border-bottom: 3px solid $gold;
+    color: $bg-light;
+    outline: none;
+    flex: 1 1;
   }
 </style>
