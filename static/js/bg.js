a70b151a8970770aa3b660f22b28afaae18aab0a
Vyne <vyne@ivy.direct>
2022-08-04 16:16:39 -0700

    repo: complete upgrade

--- static/js/bg.js
+++
@@ -1,4 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Peeking under the hood? We want you on our team! Apply now: https://pinnacle.us.org/apply");
-});
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-  mode: process.env.NODE_ENV || 'staging'
+  mode: process.env.NODE_ENV || 'staging',
+  plugins: [sveltekit()]
 })
