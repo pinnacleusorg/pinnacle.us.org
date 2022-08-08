0814043d876af50352e56f308ac149f3e4bff935
Vyne <vyne@ivy.direct>
2022-08-07 18:19:59 -0700

    feat: mobile popover nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,3 +1,13 @@
+<script>
+	import NavOverlay from "./NavOverlay.svelte";
+
+	let navOpen = false;
+
+	function toggleNav() {
+		navOpen = !navOpen;
+	}
+</script>
+
 <nav class="container-wide">
 	<img
 		src="https://static.pinnacle.us.org/2021/meta/icon.png"
@@ -9,26 +19,46 @@
 	<a href="#schedule">Schedule</a>
 	<a href="#faq">FAQ</a>
 	<a href="#contact">Contact</a>
-	<button class="mobile"
-		><svg
-			width="28"
-			height="23"
-			viewBox="0 0 28 23"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-		>
-			<line y1="1.5" x2="15" y2="1.5" stroke="#C69E60" stroke-width="3" />
-			<line
-				x1="13"
-				y1="21.5"
-				x2="28"
-				y2="21.5"
-				stroke="#C69E60"
-				stroke-width="3"
-			/>
-			<line y1="11.5" x2="28" y2="11.5" stroke="#C69E60" stroke-width="3" />
-		</svg>
+	<button class="mobile" on:click={toggleNav}>
+		{#if navOpen}
+			<svg
+				width="28"
+				height="28"
+				viewBox="0 0 28 28"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M26 2L2 26M2 2L26 26"
+					stroke="#C69E60"
+					stroke-width="3"
+					stroke-linejoin="round"
+				/>
+			</svg>
+		{:else}
+			<svg
+				width="28"
+				height="23"
+				viewBox="0 0 28 23"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<line y1="1.5" x2="15" y2="1.5" stroke="#C69E60" stroke-width="3" />
+				<line
+					x1="13"
+					y1="21.5"
+					x2="28"
+					y2="21.5"
+					stroke="#C69E60"
+					stroke-width="3"
+				/>
+				<line y1="11.5" x2="28" y2="11.5" stroke="#C69E60" stroke-width="3" />
+			</svg>
+		{/if}
 	</button>
+	{#if navOpen}
+		<NavOverlay on:close={toggleNav} />
+	{/if}
 </nav>
 
 <style lang="scss">
@@ -43,7 +73,7 @@
 		top: 0;
 		left: 0;
 		right: 0;
-		z-index: 5;
+		z-index: 50;
 
 		.nav-spacer {
 			flex: 1 1;
@@ -71,6 +101,8 @@
 		background: none;
 		border: none;
 		cursor: pointer;
+		position: relative;
+		z-index: 60;
 
 		svg {
 			height: 25px;
---
+++ src/lib/NavOverlay.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	const dispatch = createEventDispatcher();
+
+	function closeNav() {
+		dispatch("close");
+	}
+</script>
+
+<nav class="nav-overlay">
+	<a href="#about" on:click={closeNav}>About</a>
+	<a href="#schedule" on:click={closeNav}>Schedule</a>
+	<a href="#faq" on:click={closeNav}>FAQ</a>
+	<a href="#contact" on:click={closeNav}>Contact</a>
+</nav>
+
+<style lang="scss">
+	.nav-overlay {
+		@include background-white;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		row-gap: 30px;
+		font-size: 1.5rem;
+
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+	}
+</style>
