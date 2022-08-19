6f60c579a1165f79794ce30a503c46a10aa1075d
Vyne <vyne@ivy.direct>
2022-08-18 19:08:26 -0700

    repo: Upgrade packages, migrate SK routes

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,12 +1,14 @@
 <script context="module">
-	/** @type {import('@sveltejs/kit').ErrorLoad} */
-	export function load({ error, status }) {
-		return {
-			props: {
-				title: `${status}: ${error.message}`
-			}
-		};
-	}
+	throw new Error("@migration task: Replace error load function (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3293209)");
+
+	// /** @type {import('@sveltejs/kit').ErrorLoad} */
+	// export function load({ error, status }) {
+	// 	return {
+	// 		props: {
+	// 			title: `${status}: ${error.message}`
+	// 		}
+	// 	};
+	// }
 </script>
 
 <script lang="ts">
